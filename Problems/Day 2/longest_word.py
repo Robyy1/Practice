36273f1774f3cb60b76572c3a67be2ebb459bb48
Robyy1 <pirvurobert36@gmail.com>
2023-03-28 00:13:11 +0200

    Added longest word problem

--- Problems/Day 2/longest_word.py
+++ Problems/Day 2/longest_word.py
@@ -1,1 +1,17 @@
 # Write a Python program that takes in a list of words and prints the longest word in the list.
+def longest_word(words):
+    longest = "" <= == aici, a ok
+    for word in words:
+        if len(word) > len(longest):
+            longest = word
+    return longest
+
+
+word_list = ["apple", "blueberry", "blackberry", "orange", "banana", "almonds"]
+print(longest_word(word_list))
+
+
+# 1. git add .
+# 2. git commit -m "Mesajul tau"
+# 3. git push origin Practice
+
