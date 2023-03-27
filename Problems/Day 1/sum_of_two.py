d6a9c29a1391b065cab704f7d20baa9d28449721
Cipppp <pirvuciprian37@gmail.com>
2023-03-27 22:59:11 +0300

    Added longest word problem

--- Problems/Day 1/sum_of_two.py
+++ Problems/Day 1/sum_of_two.py
@@ -1,6 +1,6 @@
-num1 = 5   #intreger 1
-num2 = 10   #intreger 2
+num1 = 5  # intreger 1
+num2 = 10  # intreger 2
 
-sum_of_two_numbers = num1 + num2   #calculating the sum of the two numbers
+sum_of_two_numbers = num1 + num2  # calculating the sum of the two numbers
 
-print(sum_of_two_numbers)   #printing the value of the statement
+print(sum_of_two_numbers)  # printing the value of the statement
---
+++ Problems/Day 2/longest_word.py
@@ -0,0 +1,1 @@
+# Write a Python program that takes in a list of words and prints the longest word in the list.
