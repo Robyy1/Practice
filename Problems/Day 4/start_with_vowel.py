58e6f0f16b1f6680ba3efe5ad3104eb01ca27837
Robyy1 <81796023+Robyy1@users.noreply.github.com>
2023-03-30 22:11:51 +0200

    Add files via upload

--- Problems/Day 4/start_with_vowel.py
+++ Problems/Day 4/start_with_vowel.py
@@ -1,1 +1,12 @@
 # Write a Python program that takes in a list of words and prints the number of words in the list that start with a vowel.
+def count_vowel_words(word_list):
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    count = 0
+    for word in word_list:
+        if word[0].lower() in vowels:
+            count += 1
+    print(f"The number of words that start with a vowel is{count}")
+        
+
+words_list = ["pineapple", "orange", "mango", "apple", "banana", "pear", "watermellon" "lemon"] 
+count_vowel_words(words_list)
